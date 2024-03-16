e7f42265e5130d125213e34b1cc7ed1851ef33bd
Anaïs <43445445+amoral1@users.noreply.github.com>
2024-03-16 18:55:54 +0000

    intro

--- gov.rb
+++ gov.rb
@@ -6,6 +6,13 @@ class Gov
     end
 
     def intro
+puts "\n"
+puts "Welcome to a quick lesson on US branches of Government!"
+puts "What is your name and country of origin?"
+self.name = gets.chomp.capitalize 
+self.country = gets.chomp.capitalize 
+puts "\n"
+puts "Thank you #{self.name} from #{self.country}, please proceed with the lesson."
     end
-    
+
 end
