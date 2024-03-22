9f4efd9b0d1faa0d37aadb6d094e7cc2a1006263
Anaïs <43445445+amoral1@users.noreply.github.com>
2024-03-21 19:07:04 -0500

    Update script.rb

--- script.rb
+++ script.rb
@@ -19,6 +19,7 @@ def intro
 end
 
     def branches
+      loop do
     puts "What are the three branches of government protected by the US Constitution?"
     puts "1. Federal, Martial, Executive"
     puts "2. Judicial, Legislative, Executive"
@@ -28,14 +29,18 @@ end
     when 2 
   puts "Yes, that is correct."
         question_1
+        break
     else 
   puts "Sorry, that is incorrect, please start over."
+  branches
     end
+  end
 end
 
   
 
 def question_1
+  loop do
     puts "\n"
     puts "What is the name of the branch of government with the Supreme Court being the highest court?"
     puts "1. Judicial"
@@ -44,12 +49,16 @@ def question_1
     case choice
     when 1 
       puts "Yes, that is correct."
-        question_2
+      question_2
+      break
     when 2 
       puts "Sorry, please try again."
+    end
+  end
 end
 
     def question_2
+      loop do
   puts "\n"
   puts "What is the name of the elected bodies in the US that make up the Legislative branch?"
   puts "1. United States Senate"
@@ -59,9 +68,49 @@ end
   case choice
   when 3 
     puts "Yes, that is correct."
+    question_3
+    break
   else
     puts "Sorry, please try again."
+   end
+  end
 end
 
+def question_2
+  loop do
+  puts "\n"
+  puts "What is the name of the elected bodies in the US that make up the Legislative branch?"
+  puts "1. United States Senate"
+  puts "2. The House of Representatives"
+  puts "3. Both 1 & 2"
+  choice = gets.chomp.to_i
+  case choice
+  when 3 
+    puts "Yes, that is correct."
+    question_3
+    break
+  else
+    puts "Sorry, please try again."
+   end
+  end
 end
+
+def question_3
+  loop do
+  puts "\n"
+  puts "What is the name of the highest elected office of the Executive branch, and US government?"
+  answer = gets.chomp.to_s
+  if answer.include?("President")
+  puts "That is correct, #{name}, you have been refreshed on the function of US branches of government! Thank you for your time."
+puts "Goodbye!" 
+break
+else
+   puts "That is incorrect, please try again. Or type 0 to exit the lesson."
+  exit0 = gets.chomp.to_i 
+      exit if exit0 == 0
+   end
+  end
+end
+
+
 end
